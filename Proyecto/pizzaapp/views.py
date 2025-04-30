08f92029cb0b41ceb0ee58544df68b40089cb5b4
massimo092 <mtrangoniacosta@safareyes.es>
2025-04-30 16:30:23 +0200

    Creacion de hacer_pedido
    he creado la vista  para realizar los pedidos adomicilio

--- Proyecto/pizzaapp/views.py
+++ Proyecto/pizzaapp/views.py
@@ -7,3 +7,6 @@ def go_home(request):
     return render(request, 'home.html')
 def go_registro(request):
     return render(request, 'registro_tarjeta.html')
+
+def go_pedido(request):
+    return render(request, 'hacer_pedido.html')
