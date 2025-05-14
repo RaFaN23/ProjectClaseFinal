b26a03e64f4d71a8e699269e360cc05ff8b7a9f8
javi9234 <jpachecomontes@safareyes.es>
2025-05-14 19:54:49 +0200

    Cambios en la página home
    Ahora es visualmente mejor

--- Proyecto/pizzaapp/forms.py
+++ Proyecto/pizzaapp/forms.py
@@ -4,34 +4,33 @@ from .models import Contacto, Usuario
 
 
 class PizzaForm(forms.ModelForm):
-     class Meta:
+    class Meta:
         model = Contacto
         fields = ['nombre', 'email', 'mensaje']
 
-class RegistroFormulario(forms.ModelForm):
 
+class RegistroFormulario(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
         model = Usuario
-        fields = ['email', 'nombre','apellidos','rol', 'password']
-
+        fields = ['email', 'nombre', 'apellidos', 'rol', 'password']
 
 
 class LoginFormulario(AuthenticationForm):
-    username = forms.EmailField(label="Correo electronico")
+    username = forms.EmailField(label="Correo electrónico")
+
+    class Meta:
+        model = Usuario
+        fields = ['email', 'password']
         widgets = {
             'mensaje': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'placeholder': 'Escribe tu mensaje aquí...'}),
         }
-
         labels = {
             'nombre': 'Nombre completo',
             'email': 'Correo electrónico',
             'mensaje': 'Mensaje',
         }
-
         help_texts = {
             'email': 'Usa un correo válido como nombre@ejemplo.com',
         }
-    class Meta:
-        model = Usuario
-        fields = ['email', 'password']
